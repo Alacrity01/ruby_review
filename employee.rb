6dc3752056e4f90f731fadb3fd126d47e6b9f369
TheAlacrity <brinker.jeff@gmail.com>
2019-04-24 10:56:52 -0500

    added Food class

--- employee.rb
+++ employee.rb
@@ -13,32 +13,32 @@
 # puts "#{employee_2["first_name"]} #{employee_2["last_name"]} makes #{employee_2["salary"]} a year." # using hash
 
 class Employee
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
+  attr_reader :first_name, :last_name, :salary, :active
+  attr_writer :active
+
+
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
 
-  def first_name
-    @first_name # reader method with implicit return statement
-  end
+  # def first_name
+  #   @first_name # reader method with implicit return statement
+  # end
 
-  def last_name
-    @last_name
-  end
+  # def last_name
+  #   @last_name
+  # end
 
-  def salary
-    @salary
-  end
+  # def salary
+  #   @salary
+  # end
 
-  def active
-    @active
-  end
 
-  def active=(new_value)
-    @active = new_value
-  end
+
+
 
   def print_info
     puts "#{first_name} #{last_name} makes #{salary} a year." # using hash
@@ -49,12 +49,43 @@ class Employee
   end
 end
 
-employee_1 = Employee.new("Helena", "Bonom Carter", 80000, true)
-employee_2 = Employee.new("Peter", "Gibbons", 60000, true)
+class Manager < Employee
+  def initialize(input_options)
+    super
+    @employees = input_options[:employees]
+  end
 
-employee_1.print_info
-employee_2.print_info
+  def send_report
+    puts "Sending email..."
+    #code to send email
+    puts "Email sent"
+  end
+end
 
-p employee_2.active
-employee_2.active = false
-p employee_2.active
+employee_1 = Employee.new(
+                          first_name: "Helena", 
+                          last_name: "Bonom Carter", 
+                          salary: 80000, active: true
+                          ) # {} not required; Ruby recognizes it as a hash because it is very common
+
+employee_2 = Employee.new(
+                          first_name: "Peter", 
+                          last_name: "Gibbons", 
+                          salary: 60000, 
+                          active: true
+                          )
+
+manager = Manager.new(
+                      first_name: "Leslie",
+                      last_name: "Knope",
+                      salary: 100000,
+                      active: true,
+                      employees: [employee_1, employee_2]
+                      )
+                      
+
+
+
+p employee_1
+p employee_2
+p manager
